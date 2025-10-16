cbabb2b67ce03b9a66efe9ab66f25c0edb1e4dd3
lsy-sys <1174418578@qq.com>
2025-10-16 16:22:22 +0800

    first_commit

--- test.sql
+++ test.sql
@@ -4,6 +4,7 @@ drop table students;
 drop table teachers;
 drop table courses;
 
+-- 创建所有表
 create table students(
     id int not null ,
     name varchar(50) not null,
@@ -27,12 +28,22 @@ create table courses(
 create table students_courses(
     student_id int not null,
     course_id int not null
+                            );
+
+create table instructors_courses(
+    instructor_id int not null,
+    course_id int not null,
+    primary key (instructor_id,course_id),
+    foreign key (instructor_id) references teachers(id),
+    foreign key (course_id) references courses(id)
                              );
 
+
+
 insert into students(id,name,age) values
-                                      (1,'John',20),
-                                      (2,'Jane',21),
-                                      (3,'Bob',22),
+                                      (1,'张三',20),
+                                      (2,'李四',21),
+                                      (3,'王麻子',22),
                                       (4,'Alice',23);
 
 insert into teachers(id,name,age) values
@@ -41,9 +52,9 @@ insert into teachers(id,name,age) values
                                       (3,'Mr. Brown',45);
 
 insert into courses(id,name) values
-                                  (1,'Math'),
-                                  (2,'English'),
-                                  (3,'Science');
+                                  (1,'数学'),
+                                  (2,'英语'),
+                                  (3,'数据库');
 
 insert into students_courses(student_id,course_id) values
                                                        (1,1),
@@ -53,10 +64,31 @@ insert into students_courses(student_id,course_id) values
                                                        (3,2),
                                                        (4,3);
 
+insert into instructors_courses(instructor_id,course_id) values
+                                                             (1,1),
+                                                             (1,2),
+                                                             (2,2),
+                                                             (3,3);
 
 
 
 
+-- 一、基本的增删改查操作
+insert into students(id,name,age) values (5,'Emily',24);
+
+delete from students
+    where id = 5;
+
+update students
+    set age= age+1;
+
+
+select age from students
+    where id = 5;
+
+
+
+-- 二、外键约束问题
 -- 1. 定义级联外键
 alter table students_courses
 add constraint sc_students
@@ -97,3 +129,33 @@ select * from students;
 
 -- 增加主键的影响
 INSERT INTO students (id, name, age) VALUES (6, 'Emily', 24);
+
+
+-- 三、复杂查询
+-- 查询Mr. Smith这个老师教授的所有学生
+select name from students--学生名
+where id in (
+    select student_id from students_courses--查出选了这些课的学生
+    where course_id in (
+        select course_id from instructors_courses
+        where instructor_id = (
+            select id from teachers
+            where name = 'Mr. Smith'--查出老师教了几门课
+                                )
+        )
+    );
+
+
+-- 四，check语句，视图，索引的用法
+--1.check的用法
+alter table students
+add constraint check_age
+check (age > 18);
+
+--2.视图的创建
+create view student_view as
+    select id,name from students
+    where age > 20;
+
+--3.索引的创建
+create index index_name on students(name);
